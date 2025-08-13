21c2d284352e2d585e0e8469a2f7d15afb54f65b
iCarlSO <91926524+icarlso8@users.noreply.github.com>
2025-08-13 16:26:38 -0500

    Update infoProduct.js

--- Anunciante/Tigo/js/infoProduct.js
+++ Anunciante/Tigo/js/infoProduct.js
@@ -10,7 +10,7 @@ export function crearPlaceholder(texto) {
 }
 
 export async function cargarJerarquia() {
-  const jsonPath = "../../Anunciante/TQ/json/";
+  const jsonPath = "../../Anunciante/Tigo/json/";
   const form = document.getElementById("formulario");
 
   const section = document.createElement("fieldset");
@@ -150,6 +150,7 @@ export async function cargarJerarquia() {
   });
 
 }
+
 
 
 
