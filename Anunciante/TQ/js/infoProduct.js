3df0fa31b7d5ad031a33da459636ed23e1119997
iCarlSO <91926524+icarlso8@users.noreply.github.com>
2025-08-13 10:20:30 -0500

    Update infoProduct.js

--- Anunciante/TQ/js/infoProduct.js
+++ Anunciante/TQ/js/infoProduct.js
@@ -84,8 +84,7 @@ export async function cargarJerarquia() {
   textarea.name = "descripcion";
   textarea.rows = 3;
   textarea.placeholder = "Escribe detalles clave sobre el producto, campaña, temporada, tendencias, etc.";
-  //textarea.style.width = "492px"; "
-  //Ese width en línea en el <textarea> está forzando que ese elemento sea casi tan ancho como el contenedor.
+  //textarea.style.width = "492px"; " está forzando que ese elemento sea casi tan ancho como el contenedor.
   textarea.style.resize = "vertical";
 
   descripcionWrapper.appendChild(label);
@@ -151,6 +150,7 @@ export async function cargarJerarquia() {
   });
 
 }
+
 
 
 
